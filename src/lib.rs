e30ff496505917caa4ed4ee0c933191419f62f50
Trevor Lovell <trevorlovelldesign@gmail.com>
2021-11-07 19:41:54 -0700

    Added LdtkAsset and LdtkLoader w/ basic AssetLoader impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
-use bevy::prelude::*;
+use bevy::{
+    asset::{AssetLoader, LoadContext, LoadedAsset},
+    prelude::*,
+    reflect::TypeUuid,
+    utils::BoxedFuture,
+};
 use bevy_ecs_tilemap::prelude::*;
+use ldtk_rust::Project;
 
 pub struct LevelIdentifier {
     pub identifier: String,
+}
+
+#[derive(TypeUuid)]
+#[uuid = "ecfb87b7-9cd9-4970-8482-f2f68b770d31"]
+pub struct LdtkAsset {
+    pub project: Project,
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Hash)]
@@ -11,5 +23,27 @@ pub struct LdtkPlugin;
 impl Plugin for LdtkPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(TilemapPlugin);
+    }
+}
+
+pub struct LdtkLoader;
+
+impl AssetLoader for LdtkLoader {
+    fn load<'a>(
+        &'a self,
+        bytes: &'a [u8],
+        load_context: &'a mut LoadContext,
+    ) -> BoxedFuture<'a, anyhow::Result<()>> {
+        Box::pin(async move {
+            let project: Project = serde_json::from_slice(bytes)?;
+            let ldtk_asset = LdtkAsset { project };
+            // TODO: Support load external level files
+            load_context.set_default_asset(LoadedAsset::new(ldtk_asset));
+            Ok(())
+        })
+    }
+
+    fn extensions(&self) -> &[&str] {
+        &["ldtk"]
     }
 }

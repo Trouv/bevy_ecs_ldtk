5a9aaf9b31cf2332f1293728b5a7c8fc57c4756e
Trevor Lovell <trevorlovelldesign@gmail.com>
2021-11-07 18:14:53 -0700

    Added LdtkPlugin basics

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
+
 pub struct LevelIdentifier {
     pub identifier: String,
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Hash)]
+pub struct LdtkPlugin;
+
+impl Plugin for LdtkPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugin(TilemapPlugin);
+    }
 }

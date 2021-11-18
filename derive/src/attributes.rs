c7a9a009a9b28371824173f6bd4e59fbc2c687b0
Trevor Lovell <trevorlovelldesign@gmail.com>
2021-11-17 22:59:13 -0700

    sprite_bundle attribute now checking for # of args

--- derive/src/attributes.rs
+++ derive/src/attributes.rs
@@ -23,18 +23,20 @@ pub fn expand_sprite_bundle_attribute(
         .parse_meta()
         .expect("Cannot parse #[sprite_bundle...] attribute")
     {
-        syn::Meta::List(syn::MetaList { nested, .. }) => match nested.first().unwrap() {
-            syn::NestedMeta::Lit(syn::Lit::Str(asset)) => {
-                let asset_path = &asset.value();
+        syn::Meta::List(syn::MetaList { nested, .. }) if nested.len() == 1 => {
+            match nested.first().unwrap() {
+                syn::NestedMeta::Lit(syn::Lit::Str(asset)) => {
+                    let asset_path = &asset.value();
 
-                quote! {
-                    #field_name: bevy::prelude::SpriteBundle {
-                        material: materials.add(asset_server.load(#asset_path).into()),
-                        ..Default::default()
-                    },
-                }
+                    quote! {
+                        #field_name: bevy::prelude::SpriteBundle {
+                            material: materials.add(asset_server.load(#asset_path).into()),
+                            ..Default::default()
+                        },
+                    }
+                },
+                _ => panic!("Expected asset path as the only argument of #[sprite_bundle(...)]"),
             }
-            _ => panic!("Expected asset path as the first argument of #[sprite_bundle(...)]"),
         },
         syn::Meta::Path(_) => {
             quote! {

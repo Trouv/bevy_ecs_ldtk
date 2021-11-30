d42d737ae57acd75d389b97d7bd3dd32780f9fad
Trevor Lovell <trevorlovelldesign@gmail.com>
2021-11-29 19:47:28 -0700

    Changed attributes.rs to ldtk_entity.rs and moved expand_ldtk_entity_derive into it

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,8 +1,7 @@
 use proc_macro::TokenStream;
-use quote::quote;
 use syn;
 
-mod attributes;
+mod ldtk_entity;
 
 #[proc_macro_derive(
     LdtkEntity,
@@ -11,96 +10,5 @@ mod attributes;
 pub fn ldtk_entity_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
 
-    expand_ldtk_entity_derive(&ast)
-}
-
-static SPRITE_BUNDLE_ATTRIBUTE_NAME: &str = "sprite_bundle";
-static SPRITE_SHEET_BUNDLE_ATTRIBUTE_NAME: &str = "sprite_sheet_bundle";
-static LDTK_ENTITY_ATTRIBUTE_NAME: &str = "ldtk_entity";
-static FROM_ENTITY_INSTANCE_ATTRIBUTE_NAME: &str = "from_entity_instance";
-
-fn expand_ldtk_entity_derive(ast: &syn::DeriveInput) -> TokenStream {
-    let struct_name = &ast.ident;
-
-    let fields = match &ast.data {
-        syn::Data::Struct(syn::DataStruct {
-            fields: syn::Fields::Named(fields),
-            ..
-        }) => &fields.named,
-        _ => panic!("Expected a struct with named fields."),
-    };
-
-    let mut field_constructions = Vec::new();
-    for field in fields {
-        let field_name = field.ident.as_ref().unwrap();
-        let field_type = &field.ty;
-
-        let sprite_bundle = field
-            .attrs
-            .iter()
-            .find(|a| *a.path.get_ident().as_ref().unwrap() == SPRITE_BUNDLE_ATTRIBUTE_NAME);
-        if let Some(attribute) = sprite_bundle {
-            field_constructions.push(attributes::expand_sprite_bundle_attribute(
-                attribute, field_name, field_type,
-            ));
-            continue;
-        }
-
-        let sprite_sheet_bundle = field
-            .attrs
-            .iter()
-            .find(|a| *a.path.get_ident().as_ref().unwrap() == SPRITE_SHEET_BUNDLE_ATTRIBUTE_NAME);
-        if let Some(attribute) = sprite_sheet_bundle {
-            field_constructions.push(attributes::expand_sprite_sheet_bundle_attribute(
-                attribute, field_name, field_type,
-            ));
-            continue;
-        }
-
-        let ldtk_entity = field
-            .attrs
-            .iter()
-            .find(|a| *a.path.get_ident().as_ref().unwrap() == LDTK_ENTITY_ATTRIBUTE_NAME);
-        if let Some(attribute) = ldtk_entity {
-            field_constructions.push(attributes::expand_ldtk_entity_attribute(
-                attribute, field_name, field_type,
-            ));
-            continue;
-        }
-
-        let from_entity_instance = field
-            .attrs
-            .iter()
-            .find(|a| *a.path.get_ident().as_ref().unwrap() == FROM_ENTITY_INSTANCE_ATTRIBUTE_NAME);
-        if let Some(attribute) = from_entity_instance {
-            field_constructions.push(attributes::expand_from_entity_instance_attribute(
-                attribute, field_name, field_type,
-            ));
-            continue;
-        }
-
-        field_constructions.push(quote! {
-            #field_name: #field_type::default(),
-        });
-    }
-
-    let generics = &ast.generics;
-    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
-
-    let gen = quote! {
-        impl #impl_generics bevy_ecs_ldtk::prelude::LdtkEntity for #struct_name #ty_generics #where_clause {
-            fn bundle_entity(
-                entity_instance: &bevy_ecs_ldtk::prelude::EntityInstance,
-                tileset_map: &bevy_ecs_ldtk::prelude::TilesetMap,
-                asset_server: &bevy::prelude::AssetServer,
-                materials: &mut bevy::prelude::Assets<bevy::prelude::ColorMaterial>,
-                texture_atlases: &mut bevy::prelude::Assets<bevy::prelude::TextureAtlas>,
-            ) -> Self {
-                Self {
-                    #(#field_constructions)*
-                }
-            }
-        }
-    };
-    gen.into()
+    ldtk_entity::expand_ldtk_entity_derive(&ast)
 }

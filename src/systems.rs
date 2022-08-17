bd61844cf8977f305df15c5b8bb987a37cba4706
Trevor Lovell <trevorlovelldesign@gmail.com>
2022-08-16 21:50:50 -0600

    chore: fix clippy issues

--- src/systems.rs
+++ src/systems.rs
@@ -293,9 +293,10 @@ pub fn process_ldtk_levels(
 /// Performs the "despawning" portion of the respawn process for `Respawn` entities.
 ///
 /// This is currently an exclusive system for scheduling purposes.
-/// If we need to revert it to its non-exclusive form, copy it from
+/// If we need to revert it to its non-exclusive form, copy it from commit
 /// 90155a75acb6dea4c97bb92a724b741e693b100d
 pub fn clean_respawn_entities(world: &mut World) {
+    #[allow(clippy::type_complexity)]
     let mut system_state: SystemState<(
         Query<&Children, (With<Handle<LdtkAsset>>, With<Respawn>)>,
         Query<(Entity, &Handle<LdtkLevel>), With<Respawn>>,

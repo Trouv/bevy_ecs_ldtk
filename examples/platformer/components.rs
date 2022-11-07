3ba42f717a6c664aa7aea196b91f268915330e71
cashwasabi <marc3lox@googlemail.com>
2022-11-07 01:13:07 +0100

    refactor: move from bevy_heron to bevy_rapier2d
    - show use of `bevy_rapier2d` in platformer example
    - add `bevy_rapier2d` in cargo file and remove `heron`

--- examples/platformer/components.rs
+++ examples/platformer/components.rs
@@ -3,52 +3,55 @@ use bevy_ecs_ldtk::{prelude::*, utils::ldtk_pixel_coords_to_translation_pivoted}
 
 use std::collections::HashSet;
 
-use heron::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 #[derive(Clone, Debug, Default, Bundle, LdtkIntCell)]
 pub struct ColliderBundle {
-    pub collider: CollisionShape,
+    pub collider: Collider,
     pub rigid_body: RigidBody,
     pub velocity: Velocity,
-    pub rotation_constraints: RotationConstraints,
-    pub physic_material: PhysicMaterial,
+    pub rotation_constraints: LockedAxes,
+    pub gravity_scale: GravityScale,
+    pub friction: Friction,
+    pub density: ColliderMassProperties,
+}
+
+#[derive(Clone, Debug, Default, Bundle, LdtkIntCell)]
+pub struct SensorBundle {
+    pub collider: Collider,
+    pub sensor: Sensor,
+    pub active_events: ActiveEvents,
+    pub rotation_constraints: LockedAxes,
 }
 
 impl From<EntityInstance> for ColliderBundle {
     fn from(entity_instance: EntityInstance) -> ColliderBundle {
-        let rotation_constraints = RotationConstraints::lock();
+        let rotation_constraints = LockedAxes::ROTATION_LOCKED;
 
         match entity_instance.identifier.as_ref() {
             "Player" => ColliderBundle {
-                collider: CollisionShape::Cuboid {
-                    half_extends: Vec3::new(6., 14., 0.),
-                    border_radius: None,
-                },
+                collider: Collider::cuboid(6., 14.),
                 rigid_body: RigidBody::Dynamic,
+                friction: Friction {
+                    coefficient: 0.0,
+                    combine_rule: CoefficientCombineRule::Min,
+                },
                 rotation_constraints,
                 ..Default::default()
             },
             "Mob" => ColliderBundle {
-                collider: CollisionShape::Cuboid {
-                    half_extends: Vec3::new(5., 5., 0.),
-                    border_radius: None,
-                },
+                collider: Collider::cuboid(5., 5.),
                 rigid_body: RigidBody::KinematicVelocityBased,
                 rotation_constraints,
                 ..Default::default()
             },
             "Chest" => ColliderBundle {
-                collider: CollisionShape::Cuboid {
-                    half_extends: Vec3::new(8., 8., 0.),
-                    border_radius: None,
-                },
+                collider: Collider::cuboid(8., 8.),
                 rigid_body: RigidBody::Dynamic,
                 rotation_constraints,
-                physic_material: PhysicMaterial {
-                    friction: 0.5,
-                    density: 15.0,
-                    ..Default::default()
-                },
+                gravity_scale: GravityScale(1.0),
+                friction: Friction::new(0.5),
+                density: ColliderMassProperties::Density(15.0),
                 ..Default::default()
             },
             _ => ColliderBundle::default(),
@@ -56,22 +59,21 @@ impl From<EntityInstance> for ColliderBundle {
     }
 }
 
-impl From<IntGridCell> for ColliderBundle {
-    fn from(int_grid_cell: IntGridCell) -> ColliderBundle {
-        let rotation_constraints = RotationConstraints::lock();
+impl From<IntGridCell> for SensorBundle {
+    fn from(int_grid_cell: IntGridCell) -> SensorBundle {
+        let rotation_constraints = LockedAxes::ROTATION_LOCKED;
 
+        // ladder
         if int_grid_cell.value == 2 {
-            ColliderBundle {
-                collider: CollisionShape::Cuboid {
-                    half_extends: Vec3::new(8., 8., 0.),
-                    border_radius: None,
-                },
-                rigid_body: RigidBody::Sensor,
+            SensorBundle {
+                collider: Collider::cuboid(8., 8.),
+                sensor: Sensor,
                 rotation_constraints,
+                active_events: ActiveEvents::COLLISION_EVENTS,
                 ..Default::default()
             }
         } else {
-            ColliderBundle::default()
+            SensorBundle::default()
         }
     }
 }
@@ -150,7 +152,7 @@ pub struct Climbable;
 pub struct LadderBundle {
     #[from_int_grid_cell]
     #[bundle]
-    pub collider_bundle: ColliderBundle,
+    pub sensor_bundle: SensorBundle,
     pub climbable: Climbable,
 }
 

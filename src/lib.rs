242d4e63ace80606f9528a4e25c4970e6770fe51
Trevor Lovell <trevorlovelldesign@gmail.com>
2022-11-19 11:16:43 -0700

    docs: update to .spawn in getting started example

--- src/lib.rs
+++ src/lib.rs
@@ -28,9 +28,9 @@
 //! }
 //!
 //! fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-//!     commands.spawn_bundle(Camera2dBundle::default());
+//!     commands.spawn(Camera2dBundle::default());
 //!
-//!     commands.spawn_bundle(LdtkWorldBundle {
+//!     commands.spawn(LdtkWorldBundle {
 //!         ldtk_handle: asset_server.load("my_project.ldtk"),
 //!         ..Default::default()
 //!     });

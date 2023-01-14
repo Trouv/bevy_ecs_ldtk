cd2a433715395e94cc29f30dd18a1506a1656ac0
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-01-14 13:20:54 -0700

    docs: repair external crate links in LdtkIntCell module

--- src/app/ldtk_int_cell.rs
+++ src/app/ldtk_int_cell.rs
@@ -5,9 +5,10 @@ use crate::{
 use bevy::{ecs::system::EntityCommands, prelude::*};
 use std::{collections::HashMap, marker::PhantomData};
 
-#[allow(unused_imports)]
-use crate::app::register_ldtk_objects::RegisterLdtkObjects;
-
+/// [Bundle]: bevy::prelude::Bundle
+/// [App]: bevy::prelude::App
+/// [Component]: bevy::prelude::Component
+///
 /// Provides a constructor which can be used for spawning additional components on IntGrid tiles.
 ///
 /// After implementing this trait on a [Bundle], you can register it to spawn automatically for a
@@ -125,14 +126,15 @@ use crate::app::register_ldtk_objects::RegisterLdtkObjects;
 /// ```
 pub trait LdtkIntCell {
     /// The constructor used by the plugin when spawning additional components on IntGrid tiles.
-    /// If you need access to more of the [World], you can create a system that queries for
+    /// If you need access to more of the [World](bevy::prelude::World), you can create a system that queries for
     /// `Added<IntGridCell>`, and flesh out the entity from there, instead of implementing this
     /// trait.
     /// This is because the plugin spawns a tile with an [IntGridCell] component if the tile's
     /// value is not registered to the app.
     ///
-    /// Note: whether or not the entity is registered to the app, the plugin will insert [Transform],
-    /// [GlobalTransform], and [Parent] components to the entity **after** this bundle is inserted.
+    /// Note: whether or not the entity is registered to the app, the plugin will insert a
+    /// [SpatialBundle](bevy::prelude::SpatialBundle) to the entity **after** this bundle is
+    /// inserted.
     /// So, any custom implementations of these components within this trait will be overwritten.
     /// Furthermore, a [bevy_ecs_tilemap::tiles::TileBundle] will be inserted **before** this bundle, so
     /// be careful not to overwrite the components provided by that bundle.

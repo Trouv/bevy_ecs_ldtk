156ae8cb7c512a8458297d166891b7e2a1ec932f
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-01-23 18:54:56 -0700

    refactor!: split `RegisterLdtkObjects` into two new traits with a different naming convention (#155)
    * refactor: split RegisterLdtkObjects into LdtkEntityAppExt and LdtkIntCellAppExt
    
    * refactor: import items from new modules into app module like before
    
    * docs: update docs referring to RegisterLdtkObjects to LdtkEntityAppExt
    
    * docs: update docs referring to RegisterLdtkObjects to LdtkIntCellAppExt
    
    * refactor: update prelude imports for splitting of RegisterLdtkObjects
    
    * docs: update docs of private utility functions that mention RegisterLdtkObjects
    
    * docs: update trait docs for LdtkEntityAppExt
    
    * docs: add trait docs for LdtkIntCellAppExt
    
    * docs: add basic module-level docs for new modules
    
    * refactor: actually remove register_ldtk_objects module

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,9 +1,11 @@
 //! Types and traits for hooking into the ldtk loading process via [bevy::app::App].
 
+mod entity_app_ext;
+mod int_cell_app_ext;
 mod ldtk_entity;
 mod ldtk_int_cell;
-mod register_ldtk_objects;
 
+pub use entity_app_ext::*;
+pub use int_cell_app_ext::*;
 pub use ldtk_entity::*;
 pub use ldtk_int_cell::*;
-pub use register_ldtk_objects::*;

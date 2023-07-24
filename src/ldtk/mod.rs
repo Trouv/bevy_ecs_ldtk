abf100fa382ce05dcfc819f8683456dcc9b73b06
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-07-23 23:13:03 -0600

    refactor: rename int_grid_image to impl_definitions

--- src/ldtk/mod.rs
+++ src/ldtk/mod.rs
@@ -42,7 +42,7 @@ use crate::prelude::LdtkEntity;
 pub mod all_some_iter;
 mod color;
 mod field_instance;
-mod int_grid_image;
+mod impl_definitions;
 pub mod ldtk_fields;
 
 pub use field_instance::*;

19303d8d77d40c8d4f58e1e3d7632c4e2be78c82
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-08-01 19:51:22 -0600

    feat: add LoadedLevel type for enforcing a level has layer data

---
+++ src/ldtk/loaded_level.rs
@@ -0,0 +1,29 @@
+use crate::ldtk::Level;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+#[error("loaded levels must have non-null layer instances")]
+struct LevelNotLoaded;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+struct LoadedLevel<'a> {
+    level: &'a Level,
+}
+
+impl<'a> TryFrom<&'a Level> for LoadedLevel<'a> {
+    type Error = LevelNotLoaded;
+
+    fn try_from(level: &Level) -> Result<Self, Self::Error> {
+        if level.layer_instances.is_some() {
+            Ok(LoadedLevel { level })
+        } else {
+            Err(LevelNotLoaded)
+        }
+    }
+}
+
+impl<'a> LoadedLevel<'a> {
+    pub fn level(&self) -> &Level {
+        self.level
+    }
+}

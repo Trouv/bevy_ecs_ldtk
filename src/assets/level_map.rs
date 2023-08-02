bead8c1e4b2a7bd7d6b6067a09174e2ceaff8c44
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-08-01 23:22:18 -0600

    feat: derive more traits for LevelMap types and add constructors

--- src/assets/level_map.rs
+++ src/assets/level_map.rs
@@ -1,19 +1,41 @@
 use bevy::prelude::*;
+use derive_getters::Getters;
 use indexmap::IndexMap;
 
 use crate::assets::LdtkExternalLevel;
 
-struct InternalLevel {
-    bg_image: Handle<Image>,
+#[derive(Clone, Default, Debug, PartialEq, Eq, Getters)]
+pub struct InternalLevel {
+    bg_image: Option<Handle<Image>>,
     level_index: usize,
 }
 
-struct ExternalLevel {
-    bg_image: Handle<Image>,
+impl InternalLevel {
+    pub fn new(bg_image: Option<Handle<Image>>, level_index: usize) -> Self {
+        InternalLevel {
+            bg_image,
+            level_index,
+        }
+    }
+}
+
+#[derive(Clone, Default, Debug, PartialEq, Eq, Getters)]
+pub struct ExternalLevel {
+    bg_image: Option<Handle<Image>>,
     level_handle: Handle<LdtkExternalLevel>,
 }
 
-enum LevelMap {
+impl ExternalLevel {
+    pub fn new(bg_image: Option<Handle<Image>>, level_handle: Handle<LdtkExternalLevel>) -> Self {
+        ExternalLevel {
+            bg_image,
+            level_handle,
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum LevelMap {
     InternalLevels(IndexMap<String, InternalLevel>),
     ExternalLevels(IndexMap<String, ExternalLevel>),
 }

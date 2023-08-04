8ecbf322cbb6068e4ac5fc3823ffcefffc9b82ef
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-08-03 18:47:46 -0600

    docs: refer to LdtkProject in EntityIid docs

--- src/components/entity_iid.rs
+++ src/components/entity_iid.rs
@@ -4,10 +4,10 @@ use std::borrow::Cow;
 
 /// [`Component`] added to all [`LdtkEntity`]s by default.
 ///
-/// The `iid` stored in this component can be used to uniquely identify LDtk entities within an [`LdtkAsset`].
+/// The `iid` stored in this component can be used to uniquely identify LDtk entities within an [`LdtkProject`].
 ///
 /// [`LdtkEntity`]: crate::app::LdtkEntity
-/// [`LdtkAsset`]: crate::prelude::LdtkAsset
+/// [`LdtkProject`]: crate::prelude::LdtkProject
 #[derive(Clone, Debug, Default, Deref, Hash, Eq, PartialEq, Component, Reflect)]
 #[reflect(Component, Default, Debug)]
 pub struct EntityIid(Cow<'static, str>);

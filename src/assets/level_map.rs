fb596d736a11c7b061693c9d814dc532b63b6c38
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-08-04 23:47:12 -0600

    feat: add external_levels feature and make LevelMap a type alias depending on it

--- src/assets/level_map.rs
+++ src/assets/level_map.rs
@@ -2,14 +2,18 @@ use bevy::prelude::*;
 use derive_getters::Getters;
 use indexmap::IndexMap;
 
+#[cfg(feature = "external_levels")]
 use crate::assets::LdtkExternalLevel;
+use crate::LevelIid;
 
+#[cfg(not(feature = "external_levels"))]
 #[derive(Clone, Default, Debug, PartialEq, Eq, Getters)]
 pub struct InternalLevel {
     bg_image: Option<Handle<Image>>,
     level_index: usize,
 }
 
+#[cfg(not(feature = "external_levels"))]
 impl InternalLevel {
     pub fn new(bg_image: Option<Handle<Image>>, level_index: usize) -> Self {
         InternalLevel {
@@ -19,12 +23,14 @@ impl InternalLevel {
     }
 }
 
+#[cfg(feature = "external_levels")]
 #[derive(Clone, Default, Debug, PartialEq, Eq, Getters)]
 pub struct ExternalLevel {
     bg_image: Option<Handle<Image>>,
     level_handle: Handle<LdtkExternalLevel>,
 }
 
+#[cfg(feature = "external_levels")]
 impl ExternalLevel {
     pub fn new(bg_image: Option<Handle<Image>>, level_handle: Handle<LdtkExternalLevel>) -> Self {
         ExternalLevel {
@@ -34,8 +40,8 @@ impl ExternalLevel {
     }
 }
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-pub enum LevelMap {
-    InternalLevels(IndexMap<String, InternalLevel>),
-    ExternalLevels(IndexMap<String, ExternalLevel>),
-}
+#[cfg(not(feature = "external_levels"))]
+pub type LevelMap = IndexMap<LevelIid, InternalLevel>;
+
+#[cfg(feature = "external_levels")]
+pub type LevelMap = IndexMap<LevelIid, ExternalLevel>;

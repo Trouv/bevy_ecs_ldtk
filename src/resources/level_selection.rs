db7696ba4cc7cbd9c3f2bfee53c052c16c0819c6
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-08-13 16:36:53 -0600

    docs: update LevelSelection docs with proper links and a doctest

--- src/resources/level_selection.rs
+++ src/resources/level_selection.rs
@@ -1,14 +1,19 @@
 use crate::{ldtk::Level, LevelIid};
 use bevy::prelude::*;
 
-/// Resource for choosing which level(s) to spawn.
+/// [`Resource`] for choosing which level(s) to spawn.
 ///
 /// Updating this will despawn the current level and spawn the new one (unless they are the same).
-/// You can also load the selected level's neighbors using the [LevelSpawnBehavior] option.
+/// You can also load the selected level's neighbors using the [`LevelSpawnBehavior`] option.
 ///
-/// This resource works by updating the [LdtkWorldBundle]'s [LevelSet] component.
+/// This resource works by updating the [`LdtkWorldBundle`]'s [`LevelSet`] component.
 /// If you need more control over the spawned levels than this resource provides,
-/// you can choose not to insert this resource and interface with [LevelSet] directly instead.
+/// you can choose not to insert this resource and interface with [`LevelSet`] directly instead.
+///
+/// [`LevelSpawnBehavior`]: crate::prelude::LevelSpawnBehavior
+/// [`LdtkWorldBundle`]: crate::prelude::LdtkWorldBundle
+/// [`LevelSet`]: crate::prelude::LevelSet
+/// [`Resource`]: https://docs.rs/bevy/latest/bevy/ecs/prelude/trait.Resource.html
 #[derive(Clone, Eq, PartialEq, Debug, Resource)]
 pub enum LevelSelection {
     /// Spawn level with the given identifier.
@@ -28,10 +33,27 @@ impl Default for LevelSelection {
 }
 
 impl LevelSelection {
+    /// Construct a [`LevelSelection::Iid`] using the given iid.
+    ///
+    /// This iid only needs to implement `Into<String>`.
+    ///
+    /// # Example
+    /// ```
+    /// use bevy_ecs_ldtk::prelude::*;
+    ///
+    /// let level_selection = LevelSelection::iid("333081f6-7ac1-4fe3-bdcc-fa3704073bbe");
+    /// # assert_eq!(
+    /// #     level_selection,
+    /// #     LevelSelection::Iid(LevelIid::new("333081f6-7ac1-4fe3-bdcc-fa3704073bbe"))
+    /// # );
+    /// ```
     pub fn iid(iid: impl Into<String>) -> Self {
         LevelSelection::Iid(LevelIid::new(iid))
     }
 
+    /// Return true if the given level matches this [`LevelSelection`].
+    ///
+    /// Since levels don't inherently store their index, it needs to be provided separately.
     pub fn is_match(&self, index: &usize, level: &Level) -> bool {
         match self {
             LevelSelection::Identifier(s) => *s == level.identifier,

17e6047b29edec4d0e03c9b0e51350ad30d92715
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-08-14 23:43:23 -0600

    test: add some tests for iter_levels_with_indices

--- src/ldtk/impl_ldtk_json.rs
+++ src/ldtk/impl_ldtk_json.rs
@@ -47,3 +47,146 @@ impl LdtkJson {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::ldtk::World;
+
+    use super::*;
+
+    #[test]
+    fn iter_levels_in_root_with_indices() {
+        let mut project = LdtkJson::default();
+
+        let mut level_a = Level::default();
+        level_a.identifier = "Tutorial".to_string();
+
+        let mut level_b = Level::default();
+        level_b.identifier = "New_Beginnings".to_string();
+
+        let mut level_c = Level::default();
+        level_c.identifier = "Turning_Point".to_string();
+
+        let mut level_d = Level::default();
+        level_d.identifier = "Final_Boss".to_string();
+
+        project.levels = vec![
+            level_a.clone(),
+            level_b.clone(),
+            level_c.clone(),
+            level_d.clone(),
+        ];
+
+        let mut iter_levels_with_indices = project.iter_levels_with_indices();
+
+        assert_eq!(
+            iter_levels_with_indices.next(),
+            Some((LevelIndices::in_root(0), &level_a))
+        );
+        assert_eq!(
+            iter_levels_with_indices.next(),
+            Some((LevelIndices::in_root(1), &level_b))
+        );
+        assert_eq!(
+            iter_levels_with_indices.next(),
+            Some((LevelIndices::in_root(2), &level_c))
+        );
+        assert_eq!(
+            iter_levels_with_indices.next(),
+            Some((LevelIndices::in_root(3), &level_d))
+        );
+        assert_eq!(iter_levels_with_indices.next(), None);
+    }
+
+    #[test]
+    fn iter_levels_in_worlds_with_indices() {
+        let mut project = LdtkJson::default();
+
+        let mut level_a = Level::default();
+        level_a.identifier = "Tutorial".to_string();
+
+        let mut level_b = Level::default();
+        level_b.identifier = "New_Beginnings".to_string();
+
+        let mut level_c = Level::default();
+        level_c.identifier = "Turning_Point".to_string();
+
+        let mut level_d = Level::default();
+        level_d.identifier = "Final_Boss".to_string();
+
+        let mut world_a = World::default();
+        world_a.levels = vec![level_a.clone(), level_b.clone()];
+
+        let mut world_b = World::default();
+        world_b.levels = vec![level_c.clone(), level_d.clone()];
+
+        project.worlds = vec![world_a, world_b];
+
+        let mut iter_levels_with_indices = project.iter_levels_with_indices();
+
+        assert_eq!(
+            iter_levels_with_indices.next(),
+            Some((LevelIndices::in_world(0, 0), &level_a))
+        );
+        assert_eq!(
+            iter_levels_with_indices.next(),
+            Some((LevelIndices::in_world(0, 1), &level_b))
+        );
+        assert_eq!(
+            iter_levels_with_indices.next(),
+            Some((LevelIndices::in_world(1, 0), &level_c))
+        );
+        assert_eq!(
+            iter_levels_with_indices.next(),
+            Some((LevelIndices::in_world(1, 1), &level_d))
+        );
+        assert_eq!(iter_levels_with_indices.next(), None);
+    }
+
+    #[test]
+    fn iter_levels_with_indices_iterates_through_root_levels_first() {
+        let mut project = LdtkJson::default();
+
+        let mut level_a = Level::default();
+        level_a.identifier = "Tutorial".to_string();
+
+        let mut level_b = Level::default();
+        level_b.identifier = "New_Beginnings".to_string();
+
+        let mut level_c = Level::default();
+        level_c.identifier = "Turning_Point".to_string();
+
+        let mut level_d = Level::default();
+        level_d.identifier = "Final_Boss".to_string();
+
+        project.levels = vec![level_a.clone(), level_b.clone()];
+
+        let mut world_a = World::default();
+        world_a.levels = vec![level_c.clone()];
+
+        let mut world_b = World::default();
+        world_b.levels = vec![level_d.clone()];
+
+        project.worlds = vec![world_a, world_b];
+
+        let mut iter_levels_with_indices = project.iter_levels_with_indices();
+
+        assert_eq!(
+            iter_levels_with_indices.next(),
+            Some((LevelIndices::in_root(0), &level_a))
+        );
+        assert_eq!(
+            iter_levels_with_indices.next(),
+            Some((LevelIndices::in_root(1), &level_b))
+        );
+        assert_eq!(
+            iter_levels_with_indices.next(),
+            Some((LevelIndices::in_world(0, 0), &level_c))
+        );
+        assert_eq!(
+            iter_levels_with_indices.next(),
+            Some((LevelIndices::in_world(1, 0), &level_d))
+        );
+        assert_eq!(iter_levels_with_indices.next(), None);
+    }
+}

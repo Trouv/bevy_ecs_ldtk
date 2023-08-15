959d7e3d65e0f3c0e499ab29eb953030d738b889
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-08-14 20:30:46 -0600

    docs: add some basic docs for LevelIndices items

--- src/assets/level_metadata.rs
+++ src/assets/level_metadata.rs
@@ -1,4 +1,11 @@
+/// Indices pointing to the location of a level in an [`LdtkProject`] or [`LdtkJson`].
+///
+/// This type supports multi-world projects by storing an optional `world` index.
+/// If this is present, the level index is used within that world.
+/// If not, the level index is used in the project root's level collection.
 pub struct LevelIndices {
+    /// The index of the world the level belongs to, if the project is multi-world.
     pub world: Option<usize>,
+    /// The index of the level, either within a world or in the root of the project.
     pub level: usize,
 }

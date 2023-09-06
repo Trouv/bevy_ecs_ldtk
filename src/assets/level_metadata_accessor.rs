59d1c7f6390266db611477be192228dfc28c6256
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-09-05 20:31:13 -0600

    docs: add basic docs for LevelMetadataAccessor

--- src/assets/level_metadata_accessor.rs
+++ src/assets/level_metadata_accessor.rs
@@ -4,14 +4,25 @@ use crate::{
     LevelSelection,
 };
 
+/// Convenience methods for types that store levels and level metadata.
 pub trait LevelMetadataAccessor: RawLevelAccessor {
+    /// Returns a reference to the level metadata corresponding to the given level iid.
     fn get_level_metadata_by_iid(&self, iid: &String) -> Option<&LevelMetadata>;
 
+    /// Immutable access to a level at the given level iid.
+    ///
+    /// Note: all levels are considered [raw](RawLevelAccessor#raw-levels).
     fn get_raw_level_by_iid(&self, iid: &String) -> Option<&Level> {
         self.get_level_metadata_by_iid(iid)
             .and_then(|metadata| self.get_raw_level_at_indices(metadata.indices()))
     }
 
+    /// Find the level matching the given the given [`LevelSelection`].
+    ///
+    /// This lookup is constant for [`LevelSelection::Iid`] and [`LevelSelection::Indices`] variants.
+    /// The other variants require iterating through the levels to find the match.
+    ///
+    /// Note: all levels are considered [raw](RawLevelAccessor#raw-levels).
     fn find_raw_level_by_level_selection(
         &self,
         level_selection: &LevelSelection,

b213b460d0d921ecaeb057499c1a893a71cb9d8e
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-10-04 21:12:57 -0600

    docs: add basic docs for LevelLocale module

--- src/assets/level_locale.rs
+++ src/assets/level_locale.rs
@@ -6,15 +6,30 @@ use crate::assets::LevelMetadata;
 #[cfg(feature = "external_levels")]
 use crate::assets::ExternalLevelMetadata;
 
+/// Trait for marker types describing the location of levels.
+///
+/// Used as a trait bound to parameterize [`LdtkJsonWithMetadata`].
+/// Also provides an associated type defining the level metadata type for the locale.
+///
+/// Only implemented by [`InternalLevels`] and [`ExternalLevels`].
+///
+/// [`LdtkJsonWithMetadata`]: crate::assets::LdtkJsonWithMetadata
 pub trait LevelLocale {
+    /// Level metadata type used for this locale.
     type Metadata;
 }
 
 #[cfg(feature = "internal_levels")]
+/// Marker type for indicating an internal-levels LDtk project.
+///
+/// Used to parameterize [`LdtkJsonWithMetadata`].
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq, Reflect)]
 pub struct InternalLevels;
 
 #[cfg(feature = "internal_levels")]
+/// Marker type for indicating an external-levels LDtk projects.
+///
+/// Used to parameterize [`LdtkJsonWithMetadata`].
 impl LevelLocale for InternalLevels {
     type Metadata = LevelMetadata;
 }

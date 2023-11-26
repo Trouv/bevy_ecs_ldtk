33e7c97c268be104236abd764ce4ea720f1223d8
Trevor Lovell <trevorlovelldesign@gmail.com>
2023-11-26 15:05:07 -0700

    chore: fix clippy issues in tile-based-game example

--- examples/tile_based_game.rs
+++ examples/tile_based_game.rs
@@ -133,26 +133,23 @@ fn cache_wall_locations(
     ldtk_project_assets: Res<Assets<LdtkProject>>,
 ) {
     for level_event in level_events.iter() {
-        match level_event {
-            LevelEvent::Spawned(level_iid) => {
-                let ldtk_project = ldtk_project_assets
-                    .get(&ldtk_project_entities.single())
-                    .expect("LdtkProject should be loaded when level is spawned");
-                let level = ldtk_project
-                    .get_raw_level_by_iid(level_iid.get())
-                    .expect("spawned level should exist in project");
+        if let LevelEvent::Spawned(level_iid) = level_event {
+            let ldtk_project = ldtk_project_assets
+                .get(ldtk_project_entities.single())
+                .expect("LdtkProject should be loaded when level is spawned");
+            let level = ldtk_project
+                .get_raw_level_by_iid(level_iid.get())
+                .expect("spawned level should exist in project");
 
-                let wall_locations = walls.iter().copied().collect();
+            let wall_locations = walls.iter().copied().collect();
 
-                let new_level_walls = LevelWalls {
-                    wall_locations,
-                    level_width: level.px_wid / GRID_SIZE,
-                    level_height: level.px_hei / GRID_SIZE,
-                };
+            let new_level_walls = LevelWalls {
+                wall_locations,
+                level_width: level.px_wid / GRID_SIZE,
+                level_height: level.px_hei / GRID_SIZE,
+            };
 
-                *level_walls = new_level_walls;
-            }
-            _ => (),
+            *level_walls = new_level_walls;
         }
     }
 }

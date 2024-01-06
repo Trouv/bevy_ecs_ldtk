9bd1f359d2202a7cafd9e6d4b6e2a42dc6b8649d
Trevor Lovell <trevorlovelldesign@gmail.com>
2024-01-06 13:52:38 -0700

    docs: add initial setup for collectathon example

---
+++ examples/collectathon/main.rs
@@ -0,0 +1,30 @@
+use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(LdtkPlugin)
+        .insert_resource(LevelSelection::index(0))
+        .insert_resource(LdtkSettings {
+            level_spawn_behavior: LevelSpawnBehavior::UseWorldTranslation {
+                load_level_neighbors: true,
+            },
+            ..default()
+        })
+        .add_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let mut camera = Camera2dBundle::default();
+    camera.projection.scale = 0.5;
+    commands.spawn(camera);
+
+    let ldtk_handle = asset_server.load("collectathon.ldtk");
+
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle,
+        ..Default::default()
+    });
+}

b298cbeed55b5f13c4c739833290d84a450a3d7f
Trevor Lovell <trevorlovelldesign@gmail.com>
2024-01-06 14:25:52 -0700

    docs: add move_player system

--- examples/collectathon/player.rs
+++ examples/collectathon/player.rs
@@ -6,7 +6,8 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.register_ldtk_entity::<PlayerBundle>("Player");
+        app.add_systems(Update, move_player)
+            .register_ldtk_entity::<PlayerBundle>("Player");
     }
 }
 
@@ -19,4 +20,34 @@ struct PlayerBundle {
     wallet: Wallet,
     #[sprite_sheet_bundle]
     sprite_sheet: SpriteSheetBundle,
+}
+
+const MOVEMENT_SPEED: f32 = 96.;
+
+fn move_player(
+    mut players: Query<&mut Transform, With<Player>>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    for mut player_transform in players.iter_mut() {
+        let mut movement = Vec2::ZERO;
+
+        if input.pressed(KeyCode::W) || input.pressed(KeyCode::Up) {
+            movement += Vec2::Y;
+        }
+        if input.pressed(KeyCode::A) || input.pressed(KeyCode::Left) {
+            movement += Vec2::NEG_X;
+        }
+        if input.pressed(KeyCode::S) || input.pressed(KeyCode::Down) {
+            movement += Vec2::NEG_Y;
+        }
+        if input.pressed(KeyCode::D) || input.pressed(KeyCode::Right) {
+            movement += Vec2::X;
+        }
+
+        if movement != Vec2::ZERO {
+            player_transform.translation +=
+                movement.extend(0.) * MOVEMENT_SPEED * time.delta_seconds();
+        }
+    }
 }

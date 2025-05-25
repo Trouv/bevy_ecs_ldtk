6dc15f2630b57b62522e3b570e33518095baca9a
Trevor Lovell <trevorlovelldesign@gmail.com>
2025-05-25 16:57:41 -0600

    chore: Handle::weak_from_u128 deprecation in tests

--- src/assets/ldtk_project.rs
+++ src/assets/ldtk_project.rs
@@ -313,7 +313,7 @@ impl AssetLoader for LdtkProjectLoader {
 mod tests {
     use super::*;
     use derive_more::Constructor;
-    use fake::{Dummy, Fake, Faker};
+    use fake::{uuid::UUIDv4, Dummy, Fake};
     use rand::Rng;
 
     #[derive(Copy, Clone, Debug, Default, PartialEq, Eq, Constructor)]
@@ -335,13 +335,22 @@ mod tests {
                 .defs
                 .tilesets
                 .iter()
-                .map(|tileset| (tileset.uid, Handle::weak_from_u128(Faker.fake())))
+                .map(|tileset| {
+                    (
+                        tileset.uid,
+                        Handle::Weak(AssetId::Uuid {
+                            uuid: UUIDv4.fake(),
+                        }),
+                    )
+                })
                 .collect();
 
             LdtkProject {
                 data,
                 tileset_map,
-                int_grid_image_handle: Some(Handle::weak_from_u128(Faker.fake())),
+                int_grid_image_handle: Some(Handle::Weak(AssetId::Uuid {
+                    uuid: UUIDv4.fake(),
+                })),
             }
         }
     }

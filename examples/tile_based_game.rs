a53598a9ea1208e85fcd18e540b80f90bf5768d1
Koopa1018 <siriusbizdness@gmail.com>
2026-06-06 01:51:10 -0400

    Fix translations in tile_based_game
    The pivot code invalidates this example's assumption that the entity position is at the center of the grid.
    
    I could fix this by changing the pivots in the LDtk project...but it seems better to demonstrate how to adjust it when the pivot is wrong. I want to fix it properly by making a pivot-aware grid-position function, but that's a fair bit more complex than I can handle tonight...might even be impossible to handle in a generally-correct way.
    
    Update code references in add-gameplay-to-your-project.md as well. Need to update the textual content of this document to address the changed behavior.

--- examples/tile_based_game.rs
+++ examples/tile_based_game.rs
@@ -122,9 +122,12 @@ fn translate_grid_coords_entities(
     mut grid_coords_entities: Query<(&mut Transform, &GridCoords), Changed<GridCoords>>,
 ) {
     for (mut transform, grid_coords) in grid_coords_entities.iter_mut() {
+        // Center of grid cell
         transform.translation =
             bevy_ecs_ldtk::utils::grid_coords_to_translation(*grid_coords, IVec2::splat(GRID_SIZE))
                 .extend(transform.translation.z);
+        // offset to top-left corner to match LDtk entity definitions
+        transform.translation -= vec3(0.5, -0.5, 0.0) * GRID_SIZE as f32;
     }
 }
 

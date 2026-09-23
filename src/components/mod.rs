b84e03fabbbb075d32f9f10afb2635ebc0d9b6f4
James Gayfer <10660608+jgayfer@users.noreply.github.com>
2026-09-22 21:27:53 -0700

    Update LdtkProjectHandle to use required components
    Required components are the more ergonomic and modern way to spawn a group
    of components.
    
    Rather than needing to use a bundle with a default, required components
    let you simply define the base component, and Bevy will automatically
    add the other required components (with their defaults).
    
    This also paves the way to use LdtkProjectHandle in bsn, Bevy's scene
    notation.

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -331,6 +331,14 @@ pub(crate) struct EntityInstanceBundle {
 /// [*Anatomy of the World*](https://trouv.github.io/bevy_ecs_ldtk/v0.15.0/explanation/anatomy-of-the-world.html) <!-- x-release-please-version -->
 /// chapter of the `bevy_ecs_ldtk` book.
 #[derive(Debug, Default, Clone, Component, Reflect, Deref, DerefMut)]
+#[require(
+    LevelSet,
+    Transform,
+    GlobalTransform,
+    Visibility,
+    InheritedVisibility,
+    ViewVisibility
+)]
 #[reflect(Component)]
 pub struct LdtkProjectHandle {
     pub handle: Handle<LdtkProject>,
